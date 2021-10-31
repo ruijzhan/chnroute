1e54985a54be57d36d93d2a9500bb4e4ee16dd2e
Ruijian Zhang <qiudog825@gmail.com>
2021-10-31 23:57:14 +0800

    Revert "use dnsserver variable instead of fixed IP"
    This reverts commit b6246b690888b5da193d524f5fe71285b3b89cc2.

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@
 sh gfwlist2dnsmasq.sh -l --extra-domain-file my_gfwlist.txt -o tmp
 sed -i 's/\./\\\\./g' tmp
 sed -i 's/$/\\$"/g' tmp
-sed -i 's/^/\/ip dns static add forward-to=$dnsserver type=FWD regexp=".*/g' tmp
+sed -i 's/^/\/ip dns static add forward-to=192.168.9.1 type=FWD regexp=".*/g' tmp
 sed -i '1s/^/\/ip dns static remove [\/ip dns static find type=FWD ]\n/' tmp
 sed -i -e '$a\/ip dns cache flush' tmp
 mv tmp gfwlist.rsc
