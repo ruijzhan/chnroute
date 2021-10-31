1d18eb468da35ede01bcbd958c7e452ed1c23daf
Ruijian Zhang <qiudog825@gmail.com>
2021-10-31 23:18:24 +0800

    add CN.rsc

--- generate.sh
+++ generate.sh
@@ -6,4 +6,5 @@ sed -i 's/$/\\$"/g' tmp
 sed -i 's/^/\/ip dns static add forward-to=192.168.9.1 type=FWD regexp=".*/g' tmp
 sed -i '1s/^/\/ip dns static remove [\/ip dns static find type=FWD ]\n/' tmp
 sed -i -e '$a\/ip dns cache flush' tmp
-mv tmp gfwlist_ros.rsc
+mv tmp gfwlist.rsc
+wget http://www.iwik.org/ipcountry/mikrotik/CN -O CN.rsc
