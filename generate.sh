f4a852630ae5afc0937d81c892ce44960b814550
Ruijian Zhang <qiudog825@gmail.com>
2023-04-23 14:09:39 +0800

    update script

--- generate.sh
+++ generate.sh
@@ -6,9 +6,8 @@ set -euo pipefail  # Enable error handling and logging
 GFWLIST2DNSMASQ_SH="gfwlist2dnsmasq.sh"
 INCLUDE_LIST_TXT="include_list.txt"
 EXCLUDE_LIST_TXT="exclude_list.txt"
-TMP="tmp"
-TMP_BACKUP="tmp_backup"
-GFW_LIST="gfw_list"
+GFWLIST="gfwlist.txt"
+LIST_NAME="gfw_list"
 DNS_SERVER="\$dnsserver"
 DNS_SERVER_VAR="dnsserver"
 GFWLIST_RSC="gfwlist.rsc"
@@ -21,44 +20,35 @@ CN_URL="http://www.iwik.org/ipcountry/mikrotik/CN"
 # Define variables for command-line arguments
 extra_domain_file="--extra-domain-file $INCLUDE_LIST_TXT"
 exclude_domain_file="--exclude-domain-file $EXCLUDE_LIST_TXT"
-output_file="-o $TMP"
+output_file="-o $GFWLIST"
 
 # Run gfwlist2dnsmasq.sh and generate gfwlist.rsc
 sh "$GFWLIST2DNSMASQ_SH" -l $extra_domain_file $exclude_domain_file $output_file
 
-cp "$TMP" "$TMP_BACKUP"
+cp "$GFWLIST" "$GFWLIST_RSC"
 
 # Edit tmp using sed
-input_file="$TMP"
 sed -i "
     s/\./\\\\\\\\./g;
     s/$/\\\\$\" } on-error={}/g;
-    s/^/:do { add forward-to=${DNS_SERVER} type=FWD address-list=${GFW_LIST} regexp=\".*/g;
+    s/^/:do { add forward-to=${DNS_SERVER} type=FWD address-list=${LIST_NAME} regexp=\".*/g;
     1s/^/\/ip dns static\n/;
     1s/^/\/ip dns static remove [\/ip dns static find forward-to=${DNS_SERVER} ]\n/;
     1s/^/:global ${DNS_SERVER_VAR}\n/
-    " "$input_file"
-sed -i -e '$a\/ip dns cache flush' "$input_file"
+    " "$GFWLIST_RSC"
+sed -i -e '$a\/ip dns cache flush' "$GFWLIST_RSC"
 
-# Remove existing gfwlist.rsc if any and move tmp to gfwlist.rsc
-# rm -f "$GFWLIST_RSC"
-mv -f "$input_file" "$GFWLIST_RSC"
 
-mv "$TMP_BACKUP" "$TMP"
+cp "$GFWLIST" "$GFWLIST_V7_RSC"
 
-# Edit tmp using sed
-input_file="$TMP"
 sed -i "
     s/$/ } on-error={}/g;
-    s/^/:do { add forward-to=${DNS_SERVER} type=FWD address-list=${GFW_LIST} match-subdomain=yes name=/g;
+    s/^/:do { add forward-to=${DNS_SERVER} type=FWD address-list=${LIST_NAME} match-subdomain=yes name=/g;
     1s/^/\/ip dns static\n/;
     1s/^/\/ip dns static remove [\/ip dns static find forward-to=${DNS_SERVER} ]\n/;
     1s/^/:global ${DNS_SERVER_VAR}\n/
-    " "$input_file"
-sed -i -e '$a\/ip dns cache flush' "$input_file"
-
-# Move tmp to gfwlist_v7.rsc
-mv -f "$input_file" "$GFWLIST_V7_RSC"
+    " "$GFWLIST_V7_RSC"
+sed -i -e '$a\/ip dns cache flush' "$GFWLIST_V7_RSC"
 
 # Check if git has any changes
 if [[ $(${GIT_STATUS_CMD} | wc -l) -eq 1 ]]; then
