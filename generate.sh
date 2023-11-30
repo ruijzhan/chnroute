3f073d08b176ff1d1824b16e4474cad6fcb53640
Ruijian Zhang <qiudog825@gmail.com>
2023-11-30 22:49:43 +0800

    refactor script

--- generate.sh
+++ generate.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-set -euo pipefail # Enable error handling and logging
+set -euo pipefail
 
 export LC_ALL=POSIX
 
-# Define constants
+# Constants
 GFWLIST2DNSMASQ_SH="gfwlist2dnsmasq.sh"
 INCLUDE_LIST_TXT="include_list.txt"
 EXCLUDE_LIST_TXT="exclude_list.txt"
@@ -13,90 +13,88 @@ LIST_NAME="gfw_list"
 DNS_SERVER="\$dnsserver"
 DNS_SERVER_VAR="dnsserver"
 GFWLIST_RSC="gfwlist.rsc"
-CN_RSC="CN.rsc"
 GFWLIST_V7_RSC="gfwlist_v7.rsc"
-GIT_STATUS_CMD="git status -s"
+CN_RSC="CN.rsc"
 GFWLIST_CONF="03-gfwlist.conf"
 CN_URL="http://www.iwik.org/ipcountry/mikrotik/CN"
 
+# Function to sort files
 sort_files() {
     sort -o "$INCLUDE_LIST_TXT" "$INCLUDE_LIST_TXT"
     sort -o "$EXCLUDE_LIST_TXT" "$EXCLUDE_LIST_TXT"
 }
 
+# Function to run gfwlist2dnsmasq
 run_gfwlist2dnsmasq() {
-    # Define variables for command-line arguments
-    extra_domain_file="--extra-domain-file $INCLUDE_LIST_TXT"
-    exclude_domain_file="--exclude-domain-file $EXCLUDE_LIST_TXT"
-    output_file="-o $GFWLIST"
-
-    # Run gfwlist2dnsmasq.sh and generate gfwlist.rsc
-    sh "$GFWLIST2DNSMASQ_SH" -l $extra_domain_file $exclude_domain_file $output_file
+    sh "$GFWLIST2DNSMASQ_SH" -l --extra-domain-file "$INCLUDE_LIST_TXT" --exclude-domain-file "$EXCLUDE_LIST_TXT" -o "$GFWLIST"
 }
 
+# Function to create gfwlist resource script
 create_gfwlist_rsc() {
-    cp "$GFWLIST" "$GFWLIST_RSC"
+    local version=$1
+    local output_rsc=$2
 
-    # Edit tmp using sed
-    sed -i "
-        s/\./\\\\\\\\./g;
-        s/$/\\\\$\" } on-error={}/g;
-        s/^/:do { add forward-to=${DNS_SERVER} type=FWD address-list=${LIST_NAME} regexp=\".*/g;
-        1s/^/\/ip dns static\n/;
-        1s/^/\/ip dns static remove [\/ip dns static find forward-to=${DNS_SERVER} ]\n/;
-        1s/^/:global ${DNS_SERVER_VAR}\n/
-        " "$GFWLIST_RSC"
-    sed -i -e '$a\/ip dns cache flush' "$GFWLIST_RSC"
+    cp "$GFWLIST" "$output_rsc"
+
+    # Use a case statement to differentiate between versions
+    case "$version" in
+        v7)
+            sed -i "
+                s/$/ } on-error={}/g;
+                s/^/:do { add forward-to=${DNS_SERVER} type=FWD address-list=${LIST_NAME} match-subdomain=yes name=/g;
+                1s/^/\/ip dns static\n/;
+                1s/^/\/ip dns static remove [\/ip dns static find forward-to=${DNS_SERVER} ]\n/;
+                1s/^/:global ${DNS_SERVER_VAR}\n/
+                " "$output_rsc"
+            ;;
+        *)
+            sed -i "
+                s/\./\\\\\\\\./g;
+                s/$/\\\\$\" } on-error={}/g;
+                s/^/:do { add forward-to=${DNS_SERVER} type=FWD address-list=${LIST_NAME} regexp=\".*/g;
+                1s/^/\/ip dns static\n/;
+                1s/^/\/ip dns static remove [\/ip dns static find forward-to=${DNS_SERVER} ]\n/;
+                1s/^/:global ${DNS_SERVER_VAR}\n/
+                " "$output_rsc"
+            ;;
+    esac
+
+    sed -i -e '$a\/ip dns cache flush' "$output_rsc"
 }
 
-create_gfwlist_v7_rsc() {
-    cp "$GFWLIST" "$GFWLIST_V7_RSC"
-
-    sed -i "
-        s/$/ } on-error={}/g;
-        s/^/:do { add forward-to=${DNS_SERVER} type=FWD address-list=${LIST_NAME} match-subdomain=yes name=/g;
-        1s/^/\/ip dns static\n/;
-        1s/^/\/ip dns static remove [\/ip dns static find forward-to=${DNS_SERVER} ]\n/;
-        1s/^/:global ${DNS_SERVER_VAR}\n/
-        " "$GFWLIST_V7_RSC"
-    sed -i -e '$a\/ip dns cache flush' "$GFWLIST_V7_RSC"
-}
-
+# Function to check git status
 check_git_status() {
-    # Check if git has any changes
-    if [[ $(${GIT_STATUS_CMD} | wc -l) -eq 1 ]]; then
-        # If there's one change, checkout 03-gfwlist.conf
+    if [[ $(git status -s | wc -l) -eq 1 ]]; then
         git checkout "$GFWLIST_CONF"
     fi
 }
 
+# Function to download CN.rsc
 download_cn_rsc() {
-    # Download the CN.rsc file from http://www.iwik.org/ipcountry/mikrotik/CN,
-    # saving it as $CN_RSC. If the download fails, print an error message to stderr.
     if ! curl -sS -o "$CN_RSC" -w "%{http_code}" "$CN_URL" | grep -q '^2'; then
-        printf 'Error: failed to download %s\n' "$CN_RSC" >&2
+        echo 'Error: failed to download CN.rsc' >&2
+        return 1
     fi
 }
 
+# Function to download and decode gfwlist
 download_gfwlist() {
+    local url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
+    local output_file="gfwlist_autoproxy.txt"
 
-    # URL of the file to download
-    url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
-
-    # Name of the file to save the decoded content
-    output_file="gfwlist_autoproxy.txt"
-
-    # Download the file, decode it and save to output_file
-    curl -s "$url" | base64 --decode > "$output_file"
+    if ! curl -s "$url" | base64 --decode > "$output_file"; then
+        echo "Error: failed to download or decode gfwlist" >&2
+        return 1
+    fi
 
     echo "Decoded content saved to $output_file"
 }
 
-# Run functions sequentially
+# Main execution
 sort_files
 run_gfwlist2dnsmasq
-create_gfwlist_rsc
-create_gfwlist_v7_rsc
+create_gfwlist_rsc "default" "$GFWLIST_RSC"
+create_gfwlist_rsc "v7" "$GFWLIST_V7_RSC"
 check_git_status
 download_cn_rsc
 download_gfwlist
--- generate_cn.sh
+++ generate_cn.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf -O tmp1
-cat tmp1 | cut -d '/' -f2 > tmp
-cp tmp cnlist.txt
-sed -i 's/\./\\\\./g' tmp
-sed -i 's/$/\\$" } on-error={}/g' tmp
-sed -i 's/^/:do { add forward-to=$alidns type=FWD regexp=".*/g' tmp
-sed -i '1s/^/\/ip dns static\n/' tmp
-sed -i '1s/^/\/ip dns static remove [\/ip dns static find forward-to=$alidns ]\n/' tmp
-sed -i '1s/^/:global alidns\n/' tmp
-sed -i -e '$a\/ip dns cache flush' tmp
-mv tmp cnlist.rsc
-rm tmp1
+# Constants
+URL="https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf"
+OUTPUT_RSC="cnlist.rsc"
+
+# Download the file and check if successful
+if wget "$URL" -O - | cut -d '/' -f2 > "cnlist.txt"; then
+    # Process the file and generate cnlist.rsc
+    sed -i -e '
+        s/\./\\\\./g;
+        s/$/\\$" } on-error={}/g;
+        s/^/:do { add forward-to=$alidns type=FWD regexp="/g;
+        1s/^/\/ip dns static\n/;
+        1s/^/\/ip dns static remove [\/ip dns static find forward-to=$alidns ]\n/;
+        1s/^/:global alidns\n/;
+        $a\/ip dns cache flush
+        ' "cnlist.txt"
+
+    mv "cnlist.txt" "$OUTPUT_RSC"
+else
+    echo "Error downloading file."
+    exit 1
+fi
